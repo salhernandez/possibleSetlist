b3fa20f98ecbe1e3118f963989dda303508dcfb8
Salvador Hernandez <hernandezg.sal@gmail.com>
2019-03-08 01:16:16 -0800

    added ratios and sorting

--- possibleSetlist.js
+++ possibleSetlist.js
@@ -68,19 +68,88 @@ const getVenues = (artist) => {
     })
 };
 
+const getSongs = (venueURL) => {
+  return new Promise((resolve, reject) => {
+
+    let songNames = [];
+    axios.get(venueURL)
+    .then((response) =>{
+        if(response.status === 200) {
+          const html = response.data;
+          const $ = cheerio.load(html);
+
+          //get setlist songs
+          $('ol').find('li').find("div").find("a").map(function(i, el) {
+            // const x = el.children().first()
+            // console.log(el.children[0].data)
+            songNames.push(el.children[0].data)
+          })
+          resolve(songNames)
+        }else{
+          reject(response)
+        }
+    })
+    
+    })
+};
+
+function countSongs(allSongsArray){
+  let songsObject = {}
+  allSongsArray.map(arr => {
+    arr.map(song =>{
+      if(songsObject.hasOwnProperty(song)){
+        songsObject[song] += 1
+      }else{
+        songsObject[song] = 1
+      }
+    })
+  })
+  return songsObject
+}
+
+function getRatio(songsObject, totalVenues){
+  let ratioArr = {}
+  for (let song in songsObject) {
+    if( songsObject.hasOwnProperty(song) ) {
+      // ratioArr.push({
+      //   song: song,
+      //   ratio: songsObject[song] / totalVenues
+      // })
+      ratioArr[song] = songsObject[song] / totalVenues 
+    }
+  }
+
+  var sortable = [];
+for (var item in ratioArr) {
+    sortable.push([item, ratioArr[item]]);
+}
+
+sortable.sort(function(a, b) {
+    return b[1] - a[1];
+});
+  return sortable;
+}
+
 async function main() {
   // wait for all venue links
-
+  let venues = await getVenues("muse")
+  
   // add all songs promises based on links
+  let songsPomises = []
+  venues.map((venue) => {
+    songsPomises.push(getSongs(venue.url))
+  }); 
+
 
   //profit
+  let allSongsArray = await Promise.all(songsPomises);
+  const songCounts = countSongs(allSongsArray)
+  const generateRatios = getRatio(songCounts, venues.length);
 
-  let results = await getVenues("muse")
-  // let results = await Promise.all([
-  //   getVenues
-  // ]);
-
-  console.log(results)
+  console.log(`Total Venues: ${venues.length}`);
+  generateRatios.map((songs) => {
+    console.log(`${songs[0]}: ${songs[1] * 100}%`)
+  })
 }
 
 main();
